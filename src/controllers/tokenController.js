7ca12ac6d74d8140e68b369c3193747817902d42
unknown <st4537@utr.edu.mx>
2026-07-15 13:25:49 -0600

    token de aplicacion

--- src/controllers/tokenController.js
+++ src/controllers/tokenController.js
@@ -3,7 +3,12 @@ const jwt = require("jsonwebtoken");
 const generarToken = (req, res) => {
 
     const { appId, appSecret } = req.body;
+console.log("Body:", req.body);
 
+console.log("APP_ID:", process.env.APP_ID);
+console.log("APP_SECRET:", process.env.APP_SECRET);
+console.log("appId recibido:", appId);
+console.log("appSecret recibido:", appSecret);
     if (
         appId !== process.env.APP_ID ||
         appSecret !== process.env.APP_SECRET
